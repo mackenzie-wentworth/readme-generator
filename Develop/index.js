b934d8101e624ec288b6372d5b8a30763b2574bc
mackenzie-wentworth <mwentworth28@gmail.com>
2023-03-23 15:42:10 -0400

    added fs and inquirer 'require' modules; added objects of questions to 'questions' array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,45 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+      },
+      {
+        type: 'input',
+        message: 'What would you like to include in the Description section for your project?',
+        name: 'description',
+      },
+      {
+        type: 'input',
+        message: 'What would you like to include in the Table of Contents section?',
+        name: 'table-of-contents',
+      },
+      {
+        type: 'input',
+        message: 'What are the Installation Instructions for your project?',
+        name: 'installation-instructions',
+      },
+      {
+        type: 'input',
+        message: 'What would you like to include in the Usage Information section for your project?',
+        name: 'usage-information',
+      },
+      {
+        type: 'input',
+        message: 'What would you like to include in the Contribution Guidelines section for your project?',
+        name: 'contribution-guidelines',
+      },
+      {
+        type: 'input',
+        message: 'What would you like to include in the Test Instructions section for your project?',
+        name: 'test-instructions',
+      },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
