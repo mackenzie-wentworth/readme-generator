df54fbdd3afbc9ce4957ffe89391b28b74f51a90
mackenzie-wentworth <mwentworth28@gmail.com>
2023-03-27 10:37:49 -0400

    created functions to test 'writeToFile' and 'init' data for 'questions' array

--- index.js
+++ index.js
@@ -9,43 +9,72 @@ const questions = [
         message: 'What is the title of your project?',
         name: 'title',
       },
-      {
-        type: 'input',
-        message: 'What would you like to include in the Description section for your project?',
-        name: 'description',
-      },
-      {
-        type: 'input',
-        message: 'What would you like to include in the Table of Contents section?',
-        name: 'table-of-contents',
-      },
-      {
-        type: 'input',
-        message: 'What are the Installation Instructions for your project?',
-        name: 'installation-instructions',
-      },
-      {
-        type: 'input',
-        message: 'What would you like to include in the Usage Information section for your project?',
-        name: 'usage-information',
-      },
-      {
-        type: 'input',
-        message: 'What would you like to include in the Contribution Guidelines section for your project?',
-        name: 'contribution-guidelines',
-      },
-      {
-        type: 'input',
-        message: 'What would you like to include in the Test Instructions section for your project?',
-        name: 'test-instructions',
-      },
+      // {
+      //   type: 'input',
+      //   message: 'What would you like to include in the Description section for your project?',
+      //   name: 'description',
+      // },
+      // {
+      //   type: 'input',
+      //   message: 'What would you like to include in the Table of Contents section?',
+      //   name: 'table-of-contents',
+      // },
+      // {
+      //   type: 'input',
+      //   message: 'What are the Installation Instructions for your project?',
+      //   name: 'installation-instructions',
+      // },
+      // {
+      //   type: 'input',
+      //   message: 'What would you like to include in the Usage Information section for your project?',
+      //   name: 'usage-information',
+      // },
+      // {
+      //   type: 'input',
+      //   message: 'What would you like to include in the Contribution Guidelines section for your project?',
+      //   name: 'contribution-guidelines',
+      // },
+      // {
+      //   type: 'input',
+      //   message: 'What would you like to include in the Test Instructions section for your project?',
+      //   name: 'test-instructions',
+      // },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
+
+function init() {
+  inquirer.prompt(questions)
+      .then((userResponse) => {
+          const filename = './readME-user-data.html';
+
+          const readMeHtmlData = `<!DOCTYPE html>
+          <html lang="en">
+          <head>
+              <meta charset="UTF-8">
+              <meta http-equiv="X-UA-Compatible" content="IE=edge">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <title>Module 9 Mini Project</title>
+          </head>
+          <body>
+              <p>${userResponse.title} </p>
+          </body>
+          </html>`
+      
+
+          fs.writeFile(filename, readMeHtmlData, (err) =>
+              err ? console.log(err) : console.log('Success!')
+          );
+      });
+
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
 init();
